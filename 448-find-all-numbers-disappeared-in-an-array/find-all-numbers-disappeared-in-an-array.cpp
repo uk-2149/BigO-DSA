76cf5845fbf7cc06c93bffd1153cdec3b1e4b53a
Utkal Kumar Das <145246048+uk-2149@users.noreply.github.com>
2025-10-31 18:51:45 +0530

    Time: 5 ms (56.60%) | Memory: 53.2 MB (55.15%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        vector<int> res;
         int n = nums.size();
-        unordered_map<int, int> map;
-        for(int i=1; i<=n; i++){
-            map[i] = 0;
+
+        vector<int> res;
+        vector<bool> map(n+1, false);
+
+        for(int i : nums) {
+            map[i] = true;
         }
-        for(int i : nums){
-            map[i]++;
+
+        for(int i=1; i<=n; i++) {
+            if(!map[i]) res.push_back(i);
         }
-        for(auto& x : map){
-            if(!x.second) res.push_back(x.first);
-        }
+
         return res;
     }
 };
